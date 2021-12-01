2eef794508ac0705fa91a917b87a5068554890e4
tfwong36 <tfwong36@gmail.com>
2021-12-01 17:45:53 +0800

    [OOStepbyStep][Jason] - feat: Update Practice05

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,22 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass = -1;
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        return  this.klass != -1? super.introduce() + " I am a Teacher. I teach Class " + klass + "." : super.introduce() + " I am a Teacher. I teach No Class." ;
+    }
 }
