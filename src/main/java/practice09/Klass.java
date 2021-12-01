d0b263a06f2628801b00ecee8328c67e956f2364
tfwong36 <tfwong36@gmail.com>
2021-12-01 20:33:30 +0800

    [OO-StepByStep][Jason] - feat: implement practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,49 @@
 package practice09;
 
+import java.util.*;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private final List<Student> members= new ArrayList<>();
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public List<Student> getMembers() {
+        return members;
+    }
+
+    public String getDisplayName(){
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student student){
+        if (!members.contains(student))
+            System.out.printf("It is not one of us.\n");
+        else
+            this.leader = student;
+    }
+
+    public void appendMember(Student student){
+        if (!members.contains(student))
+            members.add(student);
+    }
+
+    public boolean isIn(Student student){
+        for (Student member : this.members) {
+            if (member.getId() == student.getId())
+                return true;
+        }
+        return false;
+    }
 }
--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,34 @@
 package practice09;
 
 public class Person {
+    private String name;
+    private int age;
+    private int id;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String introduce(){
+        return String.format("My name is %s. I am %d years old.", name, age);
+    }
 }
