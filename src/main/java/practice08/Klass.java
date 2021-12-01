a9709ee0e0129464f7e4f20ceb8364acba5ae2af
tfwong36 <tfwong36@gmail.com>
2021-12-01 19:44:34 +0800

    [OO-StepByStep][Jason] - feat: implement practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,36 @@
 package practice08;
+import java.util.*;
 
 public class Klass {
+    private int number;
+    private Student leader;
+    private final List<Student> members= new ArrayList<>();
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName(){
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student student){
+        if (!members.contains(student))
+            System.out.printf("It is not one of us.\n");
+        else
+            this.leader = student;
+    }
+
+    public void appendMember(Student student){
+        if (!members.contains(student))
+            members.add(student);
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,38 @@
 package practice08;
 
-public class Teacher {
+public class Teacher  extends Person {
+    private Klass klass;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        return this.klass != null ? super.introduce() + " I am a Teacher. I teach Class " + klass.getNumber() + "." : super.introduce() + " I am a Teacher. I teach No Class.";
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass().getNumber() == this.klass.getNumber())
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I teach %s.", this.getName(), this.getAge(), student.getName());
+        else
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I don't teach %s.", this.getName(), this.getAge(), student.getName());
+    }
 }
